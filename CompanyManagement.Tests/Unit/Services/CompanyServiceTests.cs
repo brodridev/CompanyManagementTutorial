dd36fa7e6405a4a8b304b5e646048653dc91143d
Brandon Rodriguez <brodridev@gmail.com>
2025-08-13 15:03:12 -0500

    Add tests with xunit

---
+++ CompanyManagement.Tests/Unit/Services/CompanyServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using CompanyManagement.Application.DTOs;
+using CompanyManagement.Application.Services;
+using CompanyManagement.Domain.Interfaces;
+using Moq;
+using Xunit;
+
+namespace CompanyManagement.Tests.Unit.Services;
+
+public class CompanyServiceTests
+{
+    [Fact]
+    public async Task CreateAsync_ShouldReturnNewId()
+    {
+        // Arrange
+        var repoMock = new Mock<ICompanyRepository>();
+        var uowMock = new Mock<IUnitOfWork>();
+
+        var service = new CompanyService(repoMock.Object, uowMock.Object);
+        var dto = new CreateCompanyDto("Company 1", DateTime.Now);
+
+        // Act
+        var result = await service.CreateAsync(dto);
+
+        // Assert
+        Assert.NotEqual(Guid.Empty, result);
+    }
+}
